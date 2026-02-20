571bdbeb3332e3c86ec3632ab1e24dec3b511fc3
Aliss <daw03.jesuitas.2025@gmail.com>
2026-02-20 10:36:37 +0100

    cambios con el input invisible y css

--- script.js
+++ script.js
@@ -1,163 +1,162 @@
 // load si quiero cargar imagenes o estilos externos
 
-document.addEventListener("DOMContentLoaded", () => {
+window.onload = () => {
+  // 1. DOM
 
-// 1. DOM
+  // const juego = document.getElementById("game");
+  const contenedorTexto = document.getElementById("texto-contenedor");
+  const resultadosWPM = document.getElementById("resultadosWPM");
+  const resultadosAccuracy = document.getElementById("resultadosAccuracy");
+  const btnReiniciar = document.getElementById("reload");
+  const tiempo = document.getElementById("timer");
+  const escritura = document.getElementById("escritura-contenedor");
+  const seccionResultados = document.getElementById("resultados");
 
-// const juego = document.getElementById("game");
-const contenedorTexto = document.getElementById("texto-contenedor");
-const resultadosWPM = document.getElementById("resultadosWPM");
-const resultadosAccuracy = document.getElementById("resultadosAccuracy");
-const btnReiniciar = document.getElementById("reload");
-const tiempo = document.getElementById("timer");
-const escritura = document.getElementById("escritura-contenedor");
-const seccionResultados = document.getElementById("resultados");
+  // 2. Crear variables
 
-// 2. Crear variables
+  const textoFijo =
+    "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.";
+  let temporizador = 30;
+  let intervalo = null;
+  let tiempoInicio = null;
+  let juegoFinalizado = false;
 
-const textoFijo = "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto."
-let temporizador = 30;
-let intervalo = null;
-let tiempoInicio = null;
-let juegoIniciado = false;
-let juegoFinalizado = false;
+  // 3. Función para iniciar el juego
 
-// 3. Función para iniciar el juego
-
-function iniciarJuego(){
+  function iniciarJuego() {
     // para añadir el texto en el div dnd va el texto separado por letras
-    contenedorTexto.innerHTML = textoFijo.split('').map((char) => `<span class="letra">${char}</span>`).join("");
-}
+    contenedorTexto.innerHTML = textoFijo
+      .split("")
+      .map((char) => `<span class="letra">${char}</span>`)
+      .join("");
+  }
 
-// 4. Funcion contador
+  // 4. Funcion contador
 
-function contador(){
-// contador que empieza al empezar a escribir
+  function contador() {
+    // contador que empieza al empezar a escribir
     intervalo = setInterval(() => {
-        // cuando empieza el intervalo va restando un segundo
-        temporizador--;
-        // y añado el tiempo que va quedando
-        timer.innerText = temporizador;
-        // si el tiempo llega a 0 se llama a la función finalizar el juego 
-        if(temporizador <= 0){
-            clearInterval(intervalo);
-            finalizarJuego();
-        }
+      // cuando empieza el intervalo va restando un segundo
+      temporizador--;
+      // y añado el tiempo que va quedando
+      tiempo.innerText = temporizador;
+      // si el tiempo llega a 0 se llama a la función finalizar el juego
+      if (temporizador <= 0) {
+        clearInterval(intervalo);
+        finalizarJuego();
+      }
     }, 1000);
-}
+  }
 
-// 5. Funcion empezar a escribir y comparar lo escrito con el texto y pintar
+  // 5. Funcion empezar a escribir y comparar lo escrito con el texto y pintar
 
-function pintarLetras() {
-  const letras = contenedorTexto.querySelectorAll(".letra");
-  const textoEscrito = escritura.value.split("");
+  function pintarLetras() {
+    const letras = contenedorTexto.querySelectorAll(".letra");
+    const textoEscrito = escritura.value.split("");
 
-  letras.forEach((span, index) => {
-    const caracterInput = textoEscrito[index];
+    letras.forEach((span, index) => {
+      const caracterInput = textoEscrito[index];
 
-    if (caracterInput == null) {
-      span.classList.remove("correct", "incorrect");
-    } else if (caracterInput === span.innerText) {
-      span.classList.add("correct");
-      span.classList.remove("incorrect");
-    } else {
-      span.classList.add("incorrect");
-      span.classList.remove("correct");
+      if (caracterInput == null) {
+        span.classList.remove("correct", "incorrect");
+      } else if (caracterInput === span.innerText) {
+        span.classList.add("correct");
+        span.classList.remove("incorrect");
+      } else {
+        span.classList.add("incorrect");
+        span.classList.remove("correct");
+      }
+    });
+  }
+
+  // 6. eventlisteners al escribir
+
+  escritura.addEventListener("input", () => {
+    if (juegoFinalizado) return;
+
+    // Si es la primera vez que escribe, arranca contador
+    if (!intervalo) {
+      tiempoInicio = Date.now();
+      contador();
+    }
+
+    pintarLetras();
+
+    // Si llega al final del texto antes de los 30s
+    if (escritura.value.length >= textoFijo.length) {
+      finalizarJuego();
     }
   });
-}
 
-// 5. eventlisteners al escribir
+  // 7. Funcion Calcular resultados
+  function calcularResultados() {
+    let aciertos = 0;
+    const textoEscrito = escritura.value.split("");
 
-escritura.addEventListener("input", () => {
-  if (juegoFinalizado) return;
+    textoEscrito.forEach((letra, i) => {
+      if (letra === textoFijo[i]) aciertos++;
+    });
+    // datenow() devuelve el tiempo en milisegundos
+    const segundos = (Date.now() - tiempoInicio) / 1000;
+    const minutos = segundos / 60;
 
-  // Si es la primera vez que escribe, arranca contador
-  if (!juegoIniciado) {
-    juegoIniciado = true;
-    tiempoInicio = Date.now();
-    contador();
+    // contar cuantas palabras correctas
+    const palabrasCorrectas = aciertos / 5;
+    const wpm = Math.round(palabrasCorrectas / minutos);
+
+    // % de acierto
+    const precision = Math.round((aciertos / textoEscrito.length) * 100);
+
+    resultadosWPM.innerText = wpm;
+    resultadosAccuracy.innerText = precision + "%";
   }
 
-  pintarLetras();
+  // 8. Función de fin del juego
 
-  // Si llega al final del texto antes de los 30s
-  if (escritura.value.length >= textoFijo.length) {
-    finalizarJuego();
+  // cuando el tiempo llegue a 0, para el juego
+  // se calcula el wpm  y accuracy
+  // se muestran los resultados y el boton de reinicio
+
+  function finalizarJuego() {
+    if (juegoFinalizado) return;
+    juegoFinalizado = true;
+
+    clearInterval(intervalo);
+    intervalo = null;
+
+    escritura.disabled = true;
+
+    calcularResultados();
+
+    // Mostrar resultados
+    seccionResultados.style.display = "block";
   }
-});
 
+  // 9. función de reiniciar el juego
 
-// 6. Funcion Calcular resultados
-function calcularResultados() {
-  let aciertos = 0;
-  const textoEscrito = escritura.value.split("");
-  
-  textoEscrito.forEach((letra, i) => {
-    if (letra === textoFijo[i]) aciertos++;
-  });
+  function reiniciarJuego() {
+    clearInterval(intervalo);
 
-  const segundos = (Date.now() - tiempoInicio) / 1000;
-  const minutos = segundos / 60;
+    temporizador = 30;
+    intervalo = null;
+    juegoFinalizado = false;
+    tiempoInicio = null;
 
-  // contar cuantas palabras correctas
-  const palabrasCorrectas = aciertos / 5;
-  const wpm = Math.round(palabrasCorrectas / minutos);
+    tiempo.innerText = "30";
 
-  // % de acierto 
-  const precision = Math.round((aciertos / textoEscrito.length) * 100);
+    escritura.value = "";
+    escritura.disabled = false;
 
-  resultadosWPM.innerText = wpm;
-  resultadosAccuracy.innerText = precision + "%";
-}
+    resultadosWPM.innerText = "0";
+    resultadosAccuracy.innerText = "0%";
 
-// 6. Función de fin del juego
+    seccionResultados.style.display = "none";
+    iniciarJuego();
+    escritura.focus();
+  }
 
-// cuando el tiempo llegue a 0, para el juego
-// se calcula el wpm  y accuracy
-// se muestran los resultados y el boton de reinicio
+  // 10. botón para reiniciar el juego
+  btnReiniciar.addEventListener("click", reiniciarJuego);
 
-function finalizarJuego(){
-if (juegoFinalizado) return;
-  juegoFinalizado = true;
-
-  clearInterval(intervalo);
-  intervalo = null;
-
-  escritura.disabled = true;
-
-  calcularResultados();
-
-  // Mostrar resultados
-  seccionResultados.style.display = "block";
-}
-
-// 7. boton de reiniciar el juego
-
-function reiniciarJuego(){
-clearInterval(intervalo);
-
-  temporizador = 30;
-  intervalo = null;
-  juegoIniciado = false;
-  juegoFinalizado = false;
-  tiempoInicio = null;
-
-  tiempo.innerText = "30";
-
-  escritura.value = "";
-  escritura.disabled = false;
-
-  resultadosWPM.innerText = "0";
-  resultadosAccuracy.innerText = "0%";
-
-  seccionResultados.style.display = "none";
   iniciarJuego();
- }
-
-// botón para reiniciar el juego 
-btnReiniciar.addEventListener('click', reiniciarJuego);
-
-iniciarJuego();
-// finalizarJuego()
-});
+};
